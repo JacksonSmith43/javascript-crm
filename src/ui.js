3f8e7bab986a999e6c26a2f50dd9af12c203b1dd
JacksonSmith43 <jessica444@gmx.at>
2024-11-02 10:34:37 +0100

    Company names are not missing.

--- src/ui.js
+++ src/ui.js
@@ -1,4 +1,4 @@
-import {fetchCompanies} from "./api";
+import { fetchCompanies } from "./api";
 import {
   ACCOUNT_EXECUTIVE_FIELD_NAME,
   COMPANIES_TABLE_HEADERS,
@@ -13,13 +13,24 @@ export const makeTable = async () => {
   // Print result of api call to the developer console
   // Uncomment if you need it for debugging.
   // While this method of logging variables of interest to the console is primitive, but often highly valuable debugging technique
-  // console.log(companies);
+  console.log("companies: ", companies);
+  console.log("companies[0].name: ", companies[0].name = companies[0].name);
+  console.log("companies[1].name: ", companies[1].name = companies[1].name);
+  console.log("companies[2].name: ", companies[2].name = companies[2].name);
+
+  console.log("COMPANIES_TABLE_HEADERS: ", COMPANIES_TABLE_HEADERS);
+  console.log("COMPANY_NAME_FIELD_NAME: ", COMPANY_NAME_FIELD_NAME);
+
+  let counter = 0;
+
 
   // Initialize new array and push a header row
   const companiesToDisplay = [];
   companiesToDisplay.push(COMPANIES_TABLE_HEADERS);
+  //console.log("companies: JSON:", JSON.stringify(companies));
 
   // Here we simply rearrange company fields in the order in which we want to display them in UI
+
   companies.map(company => {
     const row = [];
     row.push(
@@ -29,8 +40,30 @@ export const makeTable = async () => {
       company[REVENUE_YTD_FIELD_NAME],
       company[ACCOUNT_EXECUTIVE_FIELD_NAME]
     );
+
     companiesToDisplay.push(row);
+    console.log("row: ", row);
+    // console.log("row: ", row[0] = companies[1] = "s");
+
+
+    /* if (counter <= companiesToDisplay.length) {
+       console.log("row[1] = companies[0].name: ", row[0] = companies[0].name);
+       console.log("row[1] = companies[1].name: ", row[0] = companies[1].name);
+       console.log("row[1] = companies[2].name: ", row[1] = companies[2].name);
+ 
+       //row[counter] = companies[counter].name;
+       //console.log("row[counter] = companies[counter].name: ", row[counter] = companies[counter].name);
+       counter++;
+       console.log("counter: ", counter);
+ 
+       if (counter === companiesToDisplay.length) {
+         counter = 0;
+       }
+     }*/
+
   });
+
+  console.log("companiesToDisplay: ", companiesToDisplay);
 
   // Programmatically create html table
   const table = document.createElement("table");
@@ -43,5 +76,9 @@ export const makeTable = async () => {
       const td = tr.insertCell();
       td.innerText = column; // Take string from placeholder variable and append it to <tr> node
     });
+
+
   });
+
+
 };
