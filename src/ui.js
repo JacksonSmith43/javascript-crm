a0f6fb8778a65e1ffe901441025669e6b5392b89
JacksonSmith43 <jessica444@gmx.at>
2024-11-02 18:17:36 +0100

    Changed to 24 hour format.

--- src/ui.js
+++ src/ui.js
@@ -15,51 +15,48 @@ export const makeTable = async () => {
   // While this method of logging variables of interest to the console is primitive, but often highly valuable debugging technique
   console.log("companies: ", companies);
   console.log("companies[0].name: ", companies[0].name = companies[0].name);
-  console.log("companies[1].name: ", companies[1].name = companies[1].name);
-  console.log("companies[2].name: ", companies[2].name = companies[2].name);
 
-  console.log("COMPANIES_TABLE_HEADERS: ", COMPANIES_TABLE_HEADERS);
-  console.log("COMPANY_NAME_FIELD_NAME: ", COMPANY_NAME_FIELD_NAME);
 
-  let counter = 0;
+  /*  const date = new Date();
+    const hours = date.getUTCHours();
+    const minutes = date.getUTCMinutes();
+    const time = hours + ":" + minutes;
+  
+    companies[0].created_at = time;
+    console.log(time);
+    console.log("companies[0].created_at = time:", companies[0].created_at = time);
+  */
 
+  function formatTo24Hour(dates) {
+    if (!dates) {
+      return "";
+    }
+    const date = new Date(dates);
+    return date.toLocaleString("de-DE", {
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+      hourCycle: "h23",
+    });
+  }
 
   // Initialize new array and push a header row
   const companiesToDisplay = [];
   companiesToDisplay.push(COMPANIES_TABLE_HEADERS);
-  //console.log("companies: JSON:", JSON.stringify(companies));
 
   // Here we simply rearrange company fields in the order in which we want to display them in UI
-
   companies.map(company => {
     const row = [];
     row.push(
       company[COMPANY_NAME_FIELD_NAME],
       company[STATUS_FIELD_NAME],
-      company[CREATED_AT_FIELD_NAME],
+      formatTo24Hour(company[CREATED_AT_FIELD_NAME]),
       company[REVENUE_YTD_FIELD_NAME],
       company[ACCOUNT_EXECUTIVE_FIELD_NAME]
     );
 
     companiesToDisplay.push(row);
     console.log("row: ", row);
-    // console.log("row: ", row[0] = companies[1] = "s");
-
-
-    /* if (counter <= companiesToDisplay.length) {
-       console.log("row[1] = companies[0].name: ", row[0] = companies[0].name);
-       console.log("row[1] = companies[1].name: ", row[0] = companies[1].name);
-       console.log("row[1] = companies[2].name: ", row[1] = companies[2].name);
- 
-       //row[counter] = companies[counter].name;
-       //console.log("row[counter] = companies[counter].name: ", row[counter] = companies[counter].name);
-       counter++;
-       console.log("counter: ", counter);
- 
-       if (counter === companiesToDisplay.length) {
-         counter = 0;
-       }
-     }*/
 
   });
 
@@ -79,6 +76,7 @@ export const makeTable = async () => {
 
 
   });
+
 
 
 };
